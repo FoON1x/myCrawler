9a43321297c8b442220e2d1a2225b3e46303d88f
Eric <eric34@163.com>
2025-08-24 20:03:05 +0800

    feat(Chapter03):基本完成第三章的学习

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+    dic = {
+        'a': 1,
+        'b': 2,
+        'c':{
+            'c1': 3
+        },
+        4: 4
+    }
+    if '4' in dic:
+        print('yes')
+    else:
+        print('no')
+
+    dataset = set()
+    dataset.add(1)
+    dataset.add(2)
+    dataset.add(1)
+    dataset.add('1')
+    print(dataset)
